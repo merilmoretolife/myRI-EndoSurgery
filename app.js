8ea29258aa410ff5c94fc45be2b52bc0ccb06279
Rahul Fidai <rahul.fidai@merillife.com>
2024-06-14 12:32:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// app.js
+
 document.addEventListener('DOMContentLoaded', function () {
     if (document.getElementById('loginForm')) {
         document.getElementById('loginForm').addEventListener('submit', function (event) {
@@ -25,13 +27,11 @@ function login() {
     var password = document.getElementById('password').value;
 
     if ((username === 'rahul.fidai' && password === 'Rahul@1969') || 
-        (username === 'umesh.sharma' && password === 'Meril@123') ||
-        (username === 'asma.shaikh' && password === 'Meril@123')) {
+        (username === 'umesh.sharma' && password === 'Meril@123')) {
         var token = generateToken(16);
         localStorage.setItem('authenticated', 'true');
         localStorage.setItem('authToken', token);
         localStorage.setItem('authTime', Date.now());
-        localStorage.setItem('username', username);
         window.location.href = 'chatbot.html';
     } else {
         alert('Incorrect username or password');
@@ -49,7 +49,6 @@ function checkAuthentication() {
         localStorage.removeItem('authenticated');
         localStorage.removeItem('authToken');
         localStorage.removeItem('authTime');
-        localStorage.removeItem('username');
         window.location.href = 'index.html';
     }
 }
